2fd123a70270ca577fb749bc1f289b856df33079
unknown <rutvik.joshi@live.com>
2022-12-07 21:40:37 -0500

    first commit

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -2,6 +2,6 @@ import { createClient, cacheExchange, dedupExchange } from "urql";
 import { multipartFetchExchange } from "@urql/exchange-multipart-fetch";
 
 export const client = createClient({
-  url: `${process.env.BASE_URL}/graphql`,
+  url: `${process.env.REACT_APP_BASE_URL}/graphql`,
   exchanges: [dedupExchange, cacheExchange, multipartFetchExchange],
 });
--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
